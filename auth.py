97cab2bdab644c19eeb6f16820238a42bb612b18
Hari Sabapaty <harisabapaty@gmail.com>
2024-04-07 11:45:52 +0530

    admin login added

--- auth.py
+++ auth.py
@@ -2,9 +2,59 @@ from flask import Blueprint,Flask, render_template, request, redirect, url_for, 
 from pathlib import Path
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import render_template, request, redirect, url_for, flash
-
+import json
+from .models import Admin
 auth=Blueprint('auth',__name__)
 
 @auth.route('/login')
 def login():
     return render_template("login.html")
+
+
+def load_admin_data():
+    try:
+        # Read the JSON file and load the data back
+        with open("admin_credentials.json", "r") as json_file:
+            loaded_admin_list = json.load(json_file)
+
+        # Retrieve the singleton instance of ClinicAdminSingleton
+        admin_instance = Admin(None, None)
+
+        # Update the singleton instance with the loaded data
+        admin_instance.username = loaded_admin_list[0]['username']
+        admin_instance.password = loaded_admin_list[0]['password']
+
+        # Now you can work with the admin_instance
+
+        return [admin_instance]
+    except FileNotFoundError:
+        return None  # Return None or handle the absence of the file accordingly
+
+
+@auth.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    global adminlist
+    adminlist = load_admin_data()
+
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        hash_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+        for admin in adminlist:
+            if admin.username == username:
+                if admin.password == password:
+                    flash("Login successful.")
+                    session['admin_username'] = admin.username
+                    session['admin_password'] = admin.password
+                    return redirect(url_for('auth.admin_dashboard'))
+                else:
+                    flash("Invalid credentials. Please try again.")
+                    return render_template("admin/admin_login.html")
+        
+        flash('You are not a recognized admin user')
+        return redirect(url_for('auth.login'))
+
+    else:
+        return render_template("admin/admin_login.html")
